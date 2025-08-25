a2d76fae2d5a01564f99804f1ecd1e8d1bf1de36
waizkamboh <waizkamboh6@gmail.com>
2025-08-25 09:16:28 +0500

    text widget text decoratin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget/reorderable_list_view.dart';
+import 'package:flutter_widget/widget/text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreen(),
+      home: TextWidget(),
     );
   }
 }
---
+++ lib/widget/text.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                  'Hello World',
+                   style: TextStyle(
+                    // inherit: false, // means inherit from the nearest DefaultTextStyle above in the widget tree
+                     color: Colors.red,
+                    // backgroundColor: Colors.blue,
+                     fontSize: 30,
+                     fontWeight: FontWeight.w500,
+                     fontStyle: FontStyle.italic,
+                     letterSpacing: 10,
+                     wordSpacing: 50,
+                     textBaseline: TextBaseline.values[1],
+                     height: 10,
+                     leadingDistribution: TextLeadingDistribution.even,
+                     locale: Locale('ur'),
+                     // foreground: Paint(
+                     //
+                     // ),
+                     // background: Paint(),
+                     shadows: [
+                       Shadow(
+                         color: Colors.black,
+                         blurRadius: 10,
+                         offset: Offset.zero
+                        ),
+                       Shadow(
+                         color: Colors.yellow,
+                         blurRadius: 10,
+                         offset: Offset.zero
+                       ),
+
+                     ],
+                     decoration: TextDecoration.combine(
+                       [
+                         TextDecoration.lineThrough,
+                         TextDecoration.overline,
+                       ]
+                     ),
+                     decorationColor: Colors.red,
+                     decorationStyle: TextDecorationStyle.wavy,
+                     decorationThickness: 1,
+                     debugLabel: 'Hello',
+                     fontFamily: '',
+                     overflow: TextOverflow.visibl
+                   ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
