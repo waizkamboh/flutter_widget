5e5ddefe0ed4069806700fb824b10e6dd3f7f5f7
waizkamboh <waizkamboh6@gmail.com>
2025-09-02 18:19:32 +0500

    start index 1

--- lib/widget/list_view_builder.dart
+++ lib/widget/list_view_builder.dart
@@ -14,19 +14,15 @@ class _ListViewBuilderWidgetState extends State<ListViewBuilderWidget> {
       appBar: AppBar(
         title: Text('List View Builder'),
       ),
-      body: Column(
-        children: [
-          ListView.builder(
-            itemCount: 40,
-              itemBuilder: (context, index){
-                return ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text('Person $index'),
-                  trailing: Icon(Icons.call),
-                );
-              }
-          )
-        ],
+      body: ListView.builder(
+        itemCount: 40,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Person ${index + 1}'),
+              trailing: Icon(Icons.call),
+            );
+          }
       ),
     );
   }
