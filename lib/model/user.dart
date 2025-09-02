dba553f5550c2e844ddd4d2fd2ef9f30684c3b1d
waizkamboh <waizkamboh6@gmail.com>
2025-09-02 15:40:43 +0500

    add user model

---
+++ lib/model/user.dart
@@ -0,0 +1,106 @@
+class User {
+  final String name;
+  final String image;
+  final String lastMessage;
+  final String lastMessageTime;
+
+  User({
+    required this.name,
+    required this.image,
+    required this.lastMessage,
+    required this.lastMessageTime,
+  });
+}
+
+List<User> userData = [
+  User(
+    name: "John Doe",
+    image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    lastMessage: "Hey! How are you?",
+    lastMessageTime: "10:20 AM",
+  ),
+  User(
+    name: "Emma Watson",
+    image: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg",
+    lastMessage: "Let’s catch up tomorrow!",
+    lastMessageTime: "09:15 AM",
+  ),
+  User(
+    name: "Michael Smith",
+    image: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
+    lastMessage: "Did you check the file?",
+    lastMessageTime: "Yesterday",
+  ),
+  User(
+    name: "Sophia Johnson",
+    image: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg",
+    lastMessage: "Good night 🌙",
+    lastMessageTime: "11:45 PM",
+  ),
+  User(
+    name: "Liam Brown",
+    image: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg",
+    lastMessage: "I’ll call you in 5 minutes.",
+    lastMessageTime: "08:32 PM",
+  ),
+  User(
+    name: "Olivia Davis",
+    image: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg",
+    lastMessage: "See you soon 😊",
+    lastMessageTime: "Yesterday",
+  ),
+  User(
+    name: "Noah Wilson",
+    image: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg",
+    lastMessage: "Where are you now?",
+    lastMessageTime: "07:05 PM",
+  ),
+  User(
+    name: "Ava Martinez",
+    image: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg",
+    lastMessage: "Thanks a lot!",
+    lastMessageTime: "Yesterday",
+  ),
+  User(
+    name: "Ethan Anderson",
+    image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    lastMessage: "Can you send the notes?",
+    lastMessageTime: "02:12 PM",
+  ),
+  User(
+    name: "Isabella Thomas",
+    image: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg",
+    lastMessage: "I’m on my way 🚗",
+    lastMessageTime: "Today",
+  ),
+  User(
+    name: "James Taylor",
+    image: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
+    lastMessage: "Let’s meet at 5?",
+    lastMessageTime: "Yesterday",
+  ),
+  User(
+    name: "Mia White",
+    image: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg",
+    lastMessage: "Okay, noted 👍",
+    lastMessageTime: "Yesterday",
+  ),
+  User(
+    name: "Alexander Harris",
+    image: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg",
+    lastMessage: "Great job!",
+    lastMessageTime: "09:50 AM",
+  ),
+  User(
+    name: "Charlotte Clark",
+    image: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg",
+    lastMessage: "Talk later 💬",
+    lastMessageTime: "Yesterday",
+  ),
+  User(
+    name: "Benjamin Lewis",
+    image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    lastMessage: "Happy Birthday 🥳",
+    lastMessageTime: "07:45 AM",
+  ),
+];
