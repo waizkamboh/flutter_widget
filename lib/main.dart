4b3a5591b68350d6f09bfbfd65926f866f84bab5
waizkamboh <waizkamboh6@gmail.com>
2025-09-03 18:20:16 +0500

    list view seperator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget/widget/list_view_builder.dart';
+import 'package:flutter_widget/widget/list_view_seperator.dart';
 import 'package:flutter_widget/widget/reorderable_list_view.dart';
 import 'package:flutter_widget/widget/animated_container.dart';
 import 'package:flutter_widget/widget/bottom_navigation_bar.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ListViewBuilderWidget(),
+      home: ListViewSeparatorWidget(),
     );
   }
 }
--- lib/widget/list_view_seperator.dart
+++ lib/widget/list_view_seperator.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ListViewSeperatorWidget extends StatefulWidget {
-  const ListViewSeperatorWidget({super.key});
+class ListViewSeparatorWidget extends StatefulWidget {
+  const ListViewSeparatorWidget({super.key});
 
   @override
-  State<ListViewSeperatorWidget> createState() => _ListViewSeperatorWidgetState();
+  State<ListViewSeparatorWidget> createState() => _ListViewSeparatorWidgetState();
 }
 
-class _ListViewSeperatorWidgetState extends State<ListViewSeperatorWidget> {
+class _ListViewSeparatorWidgetState extends State<ListViewSeparatorWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('List View Seperator'),
+      ),
+      body: ListView.separated(
+        itemCount: 30,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Colors.lightBlueAccent,
+            child: Padding(
+                padding: EdgeInsets.all(20),
+              child: Text('Person ${index + 1}'),
+            ),
+          );
+        },
+        separatorBuilder: (context, index){
+          return Divider();
+        },
       ),
     );
   }
