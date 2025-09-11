894fd25742d4d2161a7119bdaca62596d8ad32ea
waizkamboh <waizkamboh6@gmail.com>
2025-09-11 16:35:32 +0500

    aspect ratio add file

---
+++ lib/widget/aspect_ratio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AspectRatioWidget extends StatefulWidget {
+  const AspectRatioWidget({super.key});
+
+  @override
+  State<AspectRatioWidget> createState() => _AspectRatioWidgetState();
+}
+
+class _AspectRatioWidgetState extends State<AspectRatioWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
