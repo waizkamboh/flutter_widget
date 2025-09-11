56b3428865bd47f6c0f343e27f8d658dc6741460
waizkamboh <waizkamboh6@gmail.com>
2025-09-11 16:21:48 +0500

    constraint box start

---
+++ lib/widget/constraint_box.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConstraintBoxWidget extends StatefulWidget {
+  const ConstraintBoxWidget({super.key});
+
+  @override
+  State<ConstraintBoxWidget> createState() => _ConstraintBoxWidgetState();
+}
+
+class _ConstraintBoxWidgetState extends State<ConstraintBoxWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
