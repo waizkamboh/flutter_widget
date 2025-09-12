a0bf29c06380b6b16b1eba4e2ca6a9b00144503c
waizkamboh <waizkamboh6@gmail.com>
2025-09-12 21:37:25 +0500

    singlechildscrollview

---
+++ lib/widget/single_child_scroll_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleChildScrollViewWidget extends StatefulWidget {
+  const SingleChildScrollViewWidget({super.key});
+
+  @override
+  State<SingleChildScrollViewWidget> createState() => _SingleChildScrollViewWidgetState();
+}
+
+class _SingleChildScrollViewWidgetState extends State<SingleChildScrollViewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
