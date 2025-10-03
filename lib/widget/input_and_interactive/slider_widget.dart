37f0b0797aabd249e57e390a30916d1551de6cab
waizkamboh <waizkamboh6@gmail.com>
2025-10-03 21:31:45 +0500

    slider widget

---
+++ lib/widget/input_and_interactive/slider_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SliderWidget extends StatefulWidget {
+  const SliderWidget({super.key});
+
+  @override
+  State<SliderWidget> createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 300,),
+
+        ],
+      ),
+    );
+  }
+}
