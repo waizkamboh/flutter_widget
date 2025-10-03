19a4fb6b7579000ca244dc6a193d815e2ef95148
waizkamboh <waizkamboh6@gmail.com>
2025-10-03 21:37:10 +0500

    fixing bug and define min and max value

--- lib/widget/input_and_interactive/slider_widget.dart
+++ lib/widget/input_and_interactive/slider_widget.dart
@@ -8,13 +8,33 @@ class SliderWidget extends StatefulWidget {
 }
 
 class _SliderWidgetState extends State<SliderWidget> {
+
+  double sliderValue = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           SizedBox(height: 300,),
-
+          Text('Sliders ?', style: TextStyle(fontSize: 30),),
+          SizedBox(height: 30,),
+          Center(
+            child: Slider(
+                activeColor: Colors.orangeAccent,
+                thumbColor: Colors.orangeAccent,
+                inactiveColor: Colors.orangeAccent.withOpacity(0.5),
+                divisions: 9,
+                label: "Rating",
+                min: 1,
+                max: 10,
+                value: sliderValue,
+                onChanged: (value) {
+                  setState(() {
+                    sliderValue = value;
+                  });
+                }
+            ),
+          )
         ],
       ),
     );
